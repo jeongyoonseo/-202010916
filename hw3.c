9e77b3e534ae35419fd667b77faa0a22cca73e9e
jeongyoonseo <68184809+jeongyoonseo@users.noreply.github.com>
2022-09-22 21:54:41 +0900

    Add files via upload

---
+++ hw3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = i; j < 5 - 1; j++)
+		{
+			printf(" ");
+		}
+		for (int k = 0; k < i * 2 + 1; k++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
